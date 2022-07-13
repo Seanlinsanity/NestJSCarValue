37358ab5f795d6587ef7ba6bc522be5a689d83d9
seanlin1016@gmail.com <seanlin1016@gmail.com>
2022-07-13 16:39:37 +0800

    setup CRUD api for users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -11,6 +12,27 @@ export class UsersController {
 
     @Post("/signup")
     createUser(@Body() body: CreateUserDto) {
-        this.usersService.create(body)
+        return this.usersService.create(body)
     }
+
+    @Get("/users/:id")
+    findUserById(@Param("id") id: string) {
+        return this.usersService.findOne(parseInt(id))
+    }
+
+    @Get("/users")
+    findUserByEmail(@Query("email") email: string) {
+        return this.usersService.find(email)
+    }
+
+    @Delete("/users/:id")
+    deleteUserById(@Param("id") id: string) {
+        return this.usersService.remove(parseInt(id))
+    }
+
+    @Patch("/users/:id")
+    patchUserById(@Param("id") id: string, @Body() body: UpdateUserDto) {
+        return this.usersService.update(parseInt(id), body)
+    }
+    
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dtos/create-user.dto';
@@ -11,7 +11,32 @@ export class UsersService {
     }
 
     create(dto: CreateUserDto) {
-        const user = this.repo.create(dto)
+        const user: User = this.repo.create(dto)
         return this.repo.save(user);
+    }
+
+    findOne(id: number) {
+        return this.repo.findOneBy({id: id})
+    }
+
+    find(email: string) {
+        return this.repo.findBy({ email })
+    }
+
+    async update(id: number, attrs: Partial<User>) {
+        const user: User = await this.findOne(id)
+        if (!user) {
+            throw new NotFoundException;
+        }
+        Object.assign(user, attrs)
+        return this.repo.save(user)
+    }
+
+    async remove(id: number) {
+        const user: User = await this.findOne(id)
+        if (!user) {
+            throw new ForbiddenException
+        }
+        return this.repo.remove(user)
     }
 }
