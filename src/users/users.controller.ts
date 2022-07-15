621148ff776952adc362c0d62c36091f5c38afc5
seanlin1016@gmail.com <seanlin1016@gmail.com>
2022-07-15 17:18:43 +0800

    implement password hashing with crypto

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UseInterceptors } from '@nestjs/common';
 import { CustomSerialize, SerializeInterceptor } from 'src/interceptors/serialization.interceptor';
+import { AuthService } from './auth.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
@@ -9,13 +10,21 @@ import { UsersService } from './users.service';
 @Controller('auth')
 export class UsersController {
 
-    constructor(private usersService: UsersService) {
+    constructor(
+        private usersService: UsersService,
+        private authService: AuthService
+    ) {
 
     }
 
     @Post("/signup")
     createUser(@Body() body: CreateUserDto) {
-        return this.usersService.create(body)
+        return this.authService.signup(body.email, body.password)
+    }
+
+    @Post("/signin")
+    signin(@Body() body: CreateUserDto) {
+        return this.authService.signin(body.email, body.password)
     }
 
     @Get("/users/:id")
